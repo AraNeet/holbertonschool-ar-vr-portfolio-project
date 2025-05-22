5fe2e2ace81cbacc75aaba1e7ba04ed3ac451419
araneet <martinezcruzaj@gmail.com>
2025-05-22 07:54:24 -0400

    Last Change I can do before Interview

--- RogueLike/Assets/ArGame/Scripts/DungeonManager/Room.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Room
-{
-    public Vector3Int position;
-    public GameObject roomObject;
-    public List<Room> connectedRooms = new List<Room>();
-} 
